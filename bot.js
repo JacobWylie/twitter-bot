d1125f312597dbb3e19bdc01557f425d6a7cf00a
JacobWylie <jacobwylie@gmail.com>
2017-08-18 17:02:00 +0200

    fixed key variables

--- bot.js
+++ bot.js
@@ -5,17 +5,17 @@ const nodeBot       = require('./nodeBot.js'),
 // Grab and retweet Node Bot when program starts
 nodeBot();
 // Retweet Node Bot every 15 mins
-setInterval(nodeBot, 1200000)
+// setInterval(nodeBot, 1200000)
 
 // Grab and retweet React Bot when program starts
 reactBot();
 // Retweet React Bot every 15 mins
-setInterval(reactBot, 1200000)
+// setInterval(reactBot, 1200000)
 
 // Grab and retweet JavaScript Bot when program starts
 javascriptBot();
 // Retweet Javascript Bot every 15 mins
-setInterval(javascriptBot, 1200000)
+// setInterval(javascriptBot, 1200000)
 
 
 
--- config.js
+++ config.js
@@ -5,9 +5,12 @@
  * access_token
  * access_token_secret
  */
+
+// heroku config:set BOT_{}_{}='your keys or tokens'
+
 module.exports = {  
-  consumer_key: 'cVPsLTFFZEmv7ccyRV60DW68Q',  
-  consumer_secret: 'HNVLN1ND4cPEXcYXpYrvm0HnFNPiDJ4XaCLLwIRncdPHSFvRY1',
-  access_token: '1573381716-6NjBBO9b7fYHTjpNswrd8aRref0ZzVQs2u6irG9',  
-  access_token_secret: 'ZEVykiLzE0U62PnLkvv1zkMZugJgdhePR5xINB3QgiQj7'
+  consumer_key: 'process.env.BOT_CONSUMER_KEY',  
+  consumer_secret: 'process.env.BOT_CONSUMER_SECRET',
+  access_token: 'process.env.BOT_ACCESS_TOKEN',  
+  access_token_secret: 'process.env.BOT_ACCESS_TOKEN_SECRET'
 }
