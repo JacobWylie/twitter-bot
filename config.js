4293b342918ddd4d9b07ea65c895abc624a38da7
JacobWylie <jacobwylie@gmail.com>
2017-08-18 15:25:25 +0200

    configs

---
+++ config.js
@@ -0,0 +1,13 @@
+//config.js
+/** TWITTER APP CONFIGURATION
+ * consumer_key
+ * consumer_secret
+ * access_token
+ * access_token_secret
+ */
+module.exports = {  
+  consumer_key: 'cVPsLTFFZEmv7ccyRV60DW68Q',  
+  consumer_secret: 'HNVLN1ND4cPEXcYXpYrvm0HnFNPiDJ4XaCLLwIRncdPHSFvRY1',
+  access_token: '1573381716-6NjBBO9b7fYHTjpNswrd8aRref0ZzVQs2u6irG9',  
+  access_token_secret: 'ZEVykiLzE0U62PnLkvv1zkMZugJgdhePR5xINB3QgiQj7'
+}
