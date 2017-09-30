7af3aa40a5c42554f0b4f4203229668292bfa2ec
JacobWylie <jacobwylie@gmail.com>
2017-09-30 12:21:23 +0200

    change radius of search parameter

--- bot.js
+++ bot.js
@@ -16,7 +16,7 @@ const retweet = () => {
         let params = {
             q: `${queries[i]}`, // Required
             count: 1,
-            geocode: '52.520007,13.404954,300mi',
+            geocode: '52.520007,13.404954,100mi',
             result_type: 'recent',
             lang: 'en'    
         }
